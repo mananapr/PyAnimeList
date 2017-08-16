7d8f95287d4a372b97b5a9c00d19a5b5fa70b7d4
mananapr <manny011@live.com>
2017-08-16 23:42:05 +0530

    Fixed PyPi Settings

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name = 'PyAnimeList',
-    packages = ['PyAnimeList'],
+    name = 'PyAnimeListt',
+    packages = ['PyAnimeListt'],
     version = '0.1',
     description = 'A CLI CLient for My Anime List',
     author = 'Manan Singh',
     author_email = 'mananapr@teknik.io',
     url = 'https://github.com/mananapr/PyAnimeList',
-    download_url = '',
+    download_url = 'https://github.com/mananapr/PyAnimeList/archive/0.1.tar.gz',
 )
