8f180390ea4616e276a503df5a906b624f93f5b5
mananapr <manny011@live.com>
2017-08-16 23:02:47 +0530

    Changed Directory Structure for PyPi

--- PyAnimeList/mal.py
+++ PyAnimeList/mal.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from definations import mal
 import webbrowser
 import shutil
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyAnimeList',
+    packages = ['PyAnimeList'],
+    version = '0.1',
+    description = 'A CLI CLient for My Anime List',
+    author = 'Manan Singh',
+    author_email = 'mananapr@teknik.io',
+    url = 'https://github.com/mananapr/PyAnimeList',
+    download_url = '',
+)
