1e832894ad9fd7969abf5c02c73c753ccef5fac5
Uyuiyu <javirmartincara@gmail.com>
2017-08-24 18:07:20 +0200

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'PyAnimeListt',
-    packages = ['PyAnimeListt'],
+    packages = ['PyAnimeList'],
     version = '0.1',
     description = 'A CLI CLient for My Anime List',
     author = 'Manan Singh',
